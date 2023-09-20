2f7d400cafad42f1e4e8cad1fd2f101f096395fe
SumitYadav <sumityadav400018@gmail.com>
2023-09-20 15:22:50 +0530

    Updates

---
+++ precache-manifest.4c8cdb5971498488c699f75d5558fd72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfac8c33930567a17052",
+    "url": "/NewsApp/static/css/main.bce66f47.chunk.css"
+  },
+  {
+    "revision": "bfac8c33930567a17052",
+    "url": "/NewsApp/static/js/main.bfac8c33.chunk.js"
+  },
+  {
+    "revision": "b5dec1235d8d1d118e4f",
+    "url": "/NewsApp/static/js/1.b5dec123.chunk.js"
+  },
+  {
+    "revision": "561173c0202b75c27916",
+    "url": "/NewsApp/static/js/2.561173c0.chunk.js"
+  },
+  {
+    "revision": "075c807d0a04bf7476e6",
+    "url": "/NewsApp/static/js/runtime~main.075c807d.js"
+  },
+  {
+    "revision": "cf152bdd591a5f5d5fe951536f0a858f",
+    "url": "/NewsApp/static/media/loading.cf152bdd.gif"
+  },
+  {
+    "revision": "fe08243c352641ecf8353d5ed6668b98",
+    "url": "/NewsApp/index.html"
+  }
+];
